1ebb201e5c6c9a5d3aba50da49fc36b06acbeef7
Rajeev Aanjana <rajeevanjana143@gmail.com>
2023-03-12 21:57:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,13 @@
 
 app.use(express.json());
 
+app.get("/api",(req,res)=>{
+    res.json({success:true,version:"v1",message:"Backend for Stack Fusion Task."})
+})
 
 app.post("/api/submit-form",formController.add)
 app.get("/api/submissions",formController.list)
 
- app.listen(5000,()=>{
+ app.listen(80,()=>{
     console.log("server started")
  })
