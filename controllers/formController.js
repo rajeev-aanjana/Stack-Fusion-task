f1e43de3d4a4f8ae41d8212cda4a8c4e0f25e01e
Rajeev Aanjana <rajeevanjana143@gmail.com>
2023-03-12 22:31:47 +0530

    bug fixes

--- controllers/formController.js
+++ controllers/formController.js
@@ -12,7 +12,7 @@ const add =async (req, res) => {
     });
   }
   console.log(typeof(body.mobile_number))
-  if (typeof(body.mobile_number) != "number" || body.mobile_number.length != 10) {
+  if (typeof(body.mobile_number) != "number" || body.mobile_number.toString().length != 10) {
     return res.status(400).json({
       success: false,
       message: "Invalid Mobile Number!",
@@ -20,7 +20,6 @@ const add =async (req, res) => {
     });
   }
   const response = await sendMail(body.email);
-  console.log(response)
   submissions.push(req.body)
   res.json({
     success: true,
